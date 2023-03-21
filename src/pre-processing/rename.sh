45a2b993f2f7f016d46a7dead096dbdd6bbe89f6
Jiyang Ye <yejiyang@gmail.com>
2023-03-21 16:21:05 +0100

    Add preprocessing script

---
+++ src/pre-processing/rename.sh
@@ -0,0 +1,26 @@
+#/usr/bin/sh
+
+# group a
+mv global_regions.csv a1_global_regions.csv
+mv region_country_index.csv a2_region_country_index.csv
+mv countries.csv a3_countries.csv
+mv country_source_index.csv a4_country_source_index.csv
+mv source.csv a5_source.csv
+
+# group b
+mv inventories.csv b1_inventories.csv
+mv inventory_summary.csv b2_inventory_summary.csv
+
+# group c
+mv apis.csv c1_apis.csv
+mv api_ready_query.csv c2_api_ready_query.csv
+mv api_results.csv c3_api_results.csv
+mv consensus_index.csv c4_consensus_index.csv
+mv consensus_score.csv c5_consensus_score.csv
+
+# group d
+mv cleanventory_chemicals.csv d1_cleanventory_chemicals.csv
+mv zeropm_chemicals.csv d2_zeropm_chemicals.csv
+
+# group e
+mv substances.csv e1_substances.csv

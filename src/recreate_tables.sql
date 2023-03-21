0d1df2a7b6838ad0ea7efbdabc8210b85b97f30b
Jiyang Ye <yejiyang@gmail.com>
2023-03-21 22:18:15 +0100

    Update sql script

--- src/recreate_tables.sql
+++ src/recreate_tables.sql
@@ -38,7 +38,13 @@ CREATE TABLE [b1_inventories_modified] (
         [source_id] integer,
         FOREIGN KEY (source_id) REFERENCES a5_source_modified(source_id)
 );
-CREATE TABLE [c1_api_ids_modified] (
+CREATE TABLE [b2_inventory_summary_modified] (
+        [inventory_id] integer,
+        [query_id] integer,
+        FOREIGN KEY (inventory_id) REFERENCES b1_inventories_modified(inventory_id),
+        FOREIGN KEY (query_id) REFERENCES c2_api_ready_query_modified(query_id)
+);
+CREATE TABLE [c1_apis_modified] (
         [api_id] integer PRIMARY KEY,
         [name] text,
         [source] text
@@ -48,9 +54,45 @@ CREATE TABLE [c2_api_ready_query_modified] (
         [query] text,
         [type] text
 );
-CREATE TABLE [b2_inventory_summary_modified] (
-        [inventory_id] integer,
+CREATE TABLE [c3_api_results_modified] (
         [query_id] integer,
-        FOREIGN KEY (inventory_id) REFERENCES b1_inventories_modified(inventory_id),
-        FOREIGN KEY (query_id) REFERENCES c2_api_ready_query_modified(query_id)
+        [api_id] integer,
+        [inchi_id] integer,
+        [rank] integer,
+        FOREIGN KEY (query_id) REFERENCES c2_api_ready_query_modified(query_id),
+        FOREIGN KEY (api_id) REFERENCES c1_apis_modified(api_id),
+        FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
+);
+CREATE TABLE [c4_consensus_index_modified] (
+        [inventory_id] integer,
+        [inchi_id] text,
+        [consensus_score] text,
+        FOREIGN KEY (inventory_id) REFERENCES b1_inventory_summary_modified(inventory_id),
+        FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
+);
+CREATE TABLE [c5_consensus_score_modified] (
+        [inventory_id] integer,
+        [inchi_id] text,
+        [inventory_id_score] text,
+        FOREIGN KEY (inventory_id) REFERENCES b1_inventory_summary_modified(inventory_id),
+        FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
+);
+CREATE TABLE [d1_cleanventory_chemicals_modified] (
+        [cleanventory_id] integer PRIMARY KEY,
+        [inventory_id] integer,
+        [inchi_id] integer,
+        FOREIGN KEY (inventory_id) REFERENCES b1_inventory_summary_modified(inventory_id),
+        FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
+);
+CREATE TABLE [d2_zeropm_chemicals_modified] (
+        [zeropm_id] integer PRIMARY KEY,
+        [inchi_id] integer,
+        FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
+);
+CREATE TABLE [e1_substances_modified] (
+        [inchi] text,
+        [inchi_id] integer PRIMARY KEY,
+        [pubchem_cid] text,
+        [ccc_cas] text,
+        [chemspider_id] text
 );

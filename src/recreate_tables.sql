6baeb5f20e36361a096135140f72193066f718cb
Jiyang Ye <yejiyang@gmail.com>
2023-03-23 21:37:06 +0100

    Re-organize the sql script

--- src/recreate_tables.sql
+++ src/recreate_tables.sql
@@ -1,14 +1,28 @@
+-- Create new tables with PRIMARY KEY and FOREIGN KEY
 CREATE TABLE [a1_global_regions_modified] (
         [region_id] integer PRIMARY KEY,
         [region] text
 );
-INSERT INTO a1_global_regions_modified SELECT * FROM a1_global_regions;
+
+CREATE TABLE [a2_region_country_index_modified] (
+        [country_id] integer,
+        [region_id] integer,
+        FOREIGN KEY (country_id) REFERENCES a3_countries_modified(country_id),
+        FOREIGN KEY ( region_id) REFERENCES a1_global_regions_modified(region_id)
+        
+);
 
 CREATE TABLE [a3_countries_modified] (
         [country_id] integer PRIMARY KEY,
         [country] text
 );
-INSERT INTO a3_countries_modified SELECT * FROM a3_countries;
+
+CREATE TABLE [a4_country_source_index_modified] (
+        [country_id] integer,
+        [source_id] integer,
+        FOREIGN KEY (country_id) REFERENCES a3_countries_modified(country_id),
+        FOREIGN KEY (source_id) REFERENCES a5_source_modified(source_id)
+);
 
 CREATE TABLE [a5_source_modified] (
         [source_id] integer PRIMARY KEY,
@@ -19,25 +33,6 @@ CREATE TABLE [a5_source_modified] (
         [country_scope] text,
         [comments] text
 );
-INSERT INTO a5_source_modified SELECT * FROM a5_source;
-
-CREATE TABLE [a2_region_country_index_modified] (
-        [country_id] integer,
-        [region_id] integer,
-        FOREIGN KEY (country_id) REFERENCES a3_countries_modified(country_id),
-        FOREIGN KEY ( region_id) REFERENCES a1_global_regions_modified(region_id)
-        
-);
-INSERT INTO a2_region_country_index_modified SELECT * FROM a2_region_country_index;
-
-CREATE TABLE [a4_country_source_index_modified] (
-        [country_id] integer,
-        [source_id] integer,
-        FOREIGN KEY (country_id) REFERENCES a3_countries_modified(country_id),
-        FOREIGN KEY (source_id) REFERENCES a5_source_modified(source_id)
-);
-INSERT INTO a4_country_source_index_modified SELECT * FROM a4_country_source_index;
-
 
 CREATE TABLE [b1_inventories_modified] (
         [inventory_id] integer PRIMARY KEY,
@@ -46,8 +41,6 @@ CREATE TABLE [b1_inventories_modified] (
         [source_id] integer,
         FOREIGN KEY (source_id) REFERENCES a5_source_modified(source_id)
 );
-INSERT INTO b1_inventories_modified SELECT * FROM b1_inventories;
-
 
 CREATE TABLE [b2_inventory_summary_modified] (
         [inventory_id] integer,
@@ -55,21 +48,19 @@ CREATE TABLE [b2_inventory_summary_modified] (
         FOREIGN KEY (inventory_id) REFERENCES b1_inventories_modified(inventory_id),
         FOREIGN KEY (query_id) REFERENCES c2_api_ready_query_modified(query_id)
 );
-INSERT INTO b2_inventory_summary_modified SELECT * FROM b2_inventory_summary;
 
 CREATE TABLE [c1_apis_modified] (
         [api_id] integer PRIMARY KEY,
         [name] text,
         [source] text
 );
-INSERT INTO c1_apis_modified SELECT * FROM c1_apis;
+
 
 CREATE TABLE [c2_api_ready_query_modified] (
         [query_id] integer PRIMARY KEY,
         [query] text,
         [type] text
 );
-INSERT INTO c2_api_ready_query_modified SELECT * FROM c2_api_ready_query;
 
 CREATE TABLE [c3_api_results_modified] (
         [query_id] integer,
@@ -80,7 +71,6 @@ CREATE TABLE [c3_api_results_modified] (
         FOREIGN KEY (api_id) REFERENCES c1_apis_modified(api_id),
         FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
 );
-INSERT INTO c3_api_results_modified SELECT * FROM c3_api_results;
 
 CREATE TABLE [c4_consensus_index_modified] (
         [inventory_id] integer,
@@ -89,7 +79,6 @@ CREATE TABLE [c4_consensus_index_modified] (
         FOREIGN KEY (inventory_id) REFERENCES b1_inventory_summary_modified(inventory_id),
         FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
 );
-INSERT INTO c4_consensus_index_modified SELECT * FROM c4_consensus_index;
 
 CREATE TABLE [c5_consensus_score_modified] (
         [inventory_id] integer,
@@ -98,7 +87,6 @@ CREATE TABLE [c5_consensus_score_modified] (
         FOREIGN KEY (inventory_id) REFERENCES b1_inventory_summary_modified(inventory_id),
         FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
 );
-INSERT INTO c5_consensus_score_modified SELECT * FROM c5_consensus_score;
 
 CREATE TABLE [d1_cleanventory_chemicals_modified] (
         [cleanventory_id] integer PRIMARY KEY,
@@ -107,14 +95,12 @@ CREATE TABLE [d1_cleanventory_chemicals_modified] (
         FOREIGN KEY (inventory_id) REFERENCES b1_inventory_summary_modified(inventory_id),
         FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
 );
-INSERT INTO d1_cleanventory_chemicals_modified SELECT * FROM d1_cleanventory_chemicals;
 
 CREATE TABLE [d2_zeropm_chemicals_modified] (
         [zeropm_id] integer PRIMARY KEY,
         [inchi_id] integer,
         FOREIGN KEY (inchi_id) REFERENCES e1_substances_modified(inchi_id)
 );
-INSERT INTO d2_zeropm_chemicals_modified SELECT * FROM d2_zeropm_chemicals;
 
 CREATE TABLE [e1_substances_modified] (
         [inchi] text,
@@ -123,4 +109,41 @@ CREATE TABLE [e1_substances_modified] (
         [ccc_cas] text,
         [chemspider_id] text
 );
+
+
+-- Copy data from old tables to new tables
+INSERT INTO a1_global_regions_modified SELECT * FROM a1_global_regions;
+INSERT INTO a2_region_country_index_modified SELECT * FROM a2_region_country_index;
+INSERT INTO a3_countries_modified SELECT * FROM a3_countries;
+INSERT INTO a4_country_source_index_modified SELECT * FROM a4_country_source_index;
+INSERT INTO a5_source_modified SELECT * FROM a5_source;
+INSERT INTO b1_inventories_modified SELECT * FROM b1_inventories;
+INSERT INTO b2_inventory_summary_modified SELECT * FROM b2_inventory_summary;
+INSERT INTO c1_apis_modified SELECT * FROM c1_apis;
+INSERT INTO c2_api_ready_query_modified SELECT * FROM c2_api_ready_query;
+INSERT INTO c3_api_results_modified SELECT * FROM c3_api_results;
+INSERT INTO c4_consensus_index_modified SELECT * FROM c4_consensus_index;
+INSERT INTO c5_consensus_score_modified SELECT * FROM c5_consensus_score;
+INSERT INTO d1_cleanventory_chemicals_modified SELECT * FROM d1_cleanventory_chemicals;
+INSERT INTO d2_zeropm_chemicals_modified SELECT * FROM d2_zeropm_chemicals;
 INSERT INTO e1_substances_modified SELECT * FROM e1_substances;
+
+
+
+
+-- Delete old tables
+DROP TABLE a1_global_regions;
+DROP TABLE a2_region_country_index;
+DROP TABLE a3_countries;
+DROP TABLE a4_country_source_index;
+DROP TABLE a5_source;
+DROP TABLE b1_inventories;
+DROP TABLE b2_inventory_summary;
+DROP TABLE c1_apis;
+DROP TABLE c2_api_ready_query;
+DROP TABLE c3_api_results;
+DROP TABLE c4_consensus_index;
+DROP TABLE c5_consensus_score;
+DROP TABLE d1_cleanventory_chemicals;
+DROP TABLE d2_zeropm_chemicals;
+DROP TABLE e1_substances;

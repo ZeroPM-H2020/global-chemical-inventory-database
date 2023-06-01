a51b6bb79b32ee55535ad2e40d5d86ffc767f845
Jiyang Ye <jiyang.ye@ngi.no>
2023-06-01 17:42:06 +0200

    update: example

--- src/examples/example-api.py
+++ src/examples/example-api.py
@@ -1,5 +1,7 @@
 import requests
 
-url = "https://database.zeropm.eu/zeropm-0~2E0~2E2/substances.json?_labels=on"
+url = (
+    "https://database.zeropm.eu/zeropm-0~2E0~2E2/country_sources_index.json?_labels=on"
+)
 response = requests.get(url)
-print(response.content)
+print(response.json())

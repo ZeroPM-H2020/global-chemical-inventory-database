b85eba202a756c88c8b78ade9aa4d751b598984e
Jiyang Ye <jiyang.ye@ngi.no>
2023-12-11 11:43:13 +0100

    Add script to build custom image

---
+++ build-custom-image-with-db.sh
@@ -0,0 +1,15 @@
+# Build docker image from specified datasette version and database file
+
+# Usage: ./build-custom-image-with-db.sh <datasette-version> <database-file>
+# Example: ./build-custom-image-with-db.sh 0.59 /path/to/database.db
+
+# using datasette from https://github.com/fgregg/datasette/commits/no_limit_csv_publish
+# using Dockerfile from https://github.com/fgregg/datasette/raw/main/Dockerfile
+# using db from ./zeropm-v0-0-3.sqlite
+# using templates from ./templates
+# using config file from ./metadata.yml
+
+# download dockerfile to custom-image folder
+# wget https://github.com/fgregg/datasette/raw/main/Dockerfile -O custom-image/Dockerfile
+
+docker build -f ./custom-image/Dockerfile . -t  yejiyang/datasette:no-limit-csv-publish-latest-v1

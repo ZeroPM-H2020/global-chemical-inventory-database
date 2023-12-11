1b03fb3794c5c6e6e117986b19f69b56ef7cb452
Jiyang Ye <jiyang.ye@ngi.no>
2023-12-11 11:47:58 +0100

    update docker file

--- build-custom-image-with-db.sh
+++ build-custom-image-with-db.sh
@@ -4,12 +4,12 @@
 # Example: ./build-custom-image-with-db.sh 0.59 /path/to/database.db
 
 # using datasette from https://github.com/fgregg/datasette/commits/no_limit_csv_publish
-# using Dockerfile from https://github.com/fgregg/datasette/raw/main/Dockerfile
+# using Dockerfile from https://github.com/simonw/datasette/raw/main/Dockerfile
 # using db from ./zeropm-v0-0-3.sqlite
 # using templates from ./templates
 # using config file from ./metadata.yml
 
 # download dockerfile to custom-image folder
-# wget https://github.com/fgregg/datasette/raw/main/Dockerfile -O custom-image/Dockerfile
+# wget https://github.com/simonw/datasette/raw/main/Dockerfile -O custom-image/Dockerfile
 
 docker build -f ./custom-image/Dockerfile . -t  yejiyang/datasette:no-limit-csv-publish-latest-v1
